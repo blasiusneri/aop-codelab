79933e886cc1ed35a4472a72474e8c6d669e535b
blasius neri <blasius.neri@gmail.com>
2026-02-28 09:17:04 +0700

    Initial commit: Spring Boot 4 + Kotlin + Maven + SwaggerUI + Docker

---
+++ src/main/kotlin/com/example/aopcodelab/AopCodelabApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aopcodelab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AopCodelabApplication
+
+fun main(args: Array<String>) {
+	runApplication<AopCodelabApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/aopcodelab/AopCodelabApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.aopcodelab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AopCodelabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
